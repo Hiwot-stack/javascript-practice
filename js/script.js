950ec2be3c1e789ba9e0c24ba6ce7775b1e1f51b
hiwot-stack <hiwotkebede26@gmail.com>
2021-11-01 23:40:08 -0400

    adding comment on scripts

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//This is my first comment
+/*I love challenge 
+because challenge is an opportunity to learn new thing */
+console.log("My name Hiwot Gebre")
+console.log("My favourite food is Shiro")
