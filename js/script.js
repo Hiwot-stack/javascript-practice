1ea58e05731eca33ad9857195af380c69fb129c8
hiwot-stack <hiwotkebede26@gmail.com>
2021-11-03 22:26:53 -0400

    done day-2 daily challenge assignment

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 because challenge is an opportunity to learn new thing */
 console.log("My name Hiwot Gebre")
 console.log("My favourite food is Shiro")
+let word = "Supercalifragilisticexpialidocious"
+console.log(word.length);
+let num = 55;
+console.log(num);
+let bigInt = 50n;
+console.log(bigInt);
+console.log(typeof('num'));
+console.log(typeof(bigInt))
